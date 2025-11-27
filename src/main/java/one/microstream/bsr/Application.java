7f25bd497bdf2bdcfa6cb0a9b5b1a630587cef01
Manuel Stör <m.stoer@microstream.one>
2025-11-27 12:09:19 +0100

    Add missing dependencies

--- src/main/java/one/microstream/bsr/Application.java
+++ src/main/java/one/microstream/bsr/Application.java
@@ -1,14 +1,11 @@
 package one.microstream.bsr;
 
-import org.slf4j.LoggerFactory;
-
 import io.micronaut.runtime.Micronaut;
 
 public class Application
 {
 	public static void main(final String[] args)
 	{
-		LoggerFactory.getLogger(Application.class).info("Yeppers");
 		Micronaut.run(Application.class, args);
 	}
 }
